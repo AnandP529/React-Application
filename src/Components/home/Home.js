a91e402a4673d02e8cf000ace3e5ed565f828c80
Princo <princo123@gmail.com>
2026-03-03 18:16:03 +0530

    changed component

--- src/Components/home/Home.js
+++ src/Components/home/Home.js
@@ -7,9 +7,8 @@ class Home extends React.Component{
                 <div className="landing-page">
                     <div className="wrapper">
                         <div className="d-flex flex-column justify-content-center text-center align-items-center h-100">
-                            <h2 className="display-3">Welcome to React Routing</h2>
+                            <h2 className="display-3">Welcome to React Application</h2>
                             <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error perferendis nisi obcaecati, quo nobis beatae reprehenderit impedit dolorum molestias quos delectus hic ab maiores eligendi minus! Maxime labore soluta praesentium!</p>
-
                         </div>
                     </div>
                 </div>
